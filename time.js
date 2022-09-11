65f8afce3d5b103fcd4f0b63389d246062ae50ff
helloyunnnn <109408944+helloyunnnn@users.noreply.github.com>
2022-09-12 05:16:08 +0900

    Update time.js

--- time.js
+++ time.js
@@ -2,9 +2,13 @@ const greetings = document.querySelector("#greetings span");
 const time = document.querySelector("#time-weather__time");
 const ampm = document.querySelector("#time-weather__ampm");
 
-function setGreetings(hour) {
+function setGreetings() {
+  const date = new Date();
+  const hour = date.getHours();
+
   const name = localStorage.getItem(ITEM_USER_NAME);
   let greetingsText = "";
+  console.log(hour);
   if (hour < 5) greetingsText = "Good Night";
   else if (hour < 11) greetingsText = "Good Morning";
   else if (hour < 5) greetingsText = "Good Afternoon";
@@ -17,13 +21,15 @@ function setGreetings(hour) {
 function setTimeText() {
   const date = new Date();
   let hour = date.getHours();
+  const min = date.getMinutes();
+  if (min === 0) setGreetings();
 
   const ampmText = hour < 12 ? "AM" : "PM";
   if (hour > 12) hour -= 12;
   else if (hour === 0) hour = 12;
 
   const hourText = String(hour).padStart(2, "0");
-  const minText = String(date.getMinutes()).padStart(2, "0");
+  const minText = String(min).padStart(2, "0");
 
   ampm.innerText = ampmText;
   time.innerText = `${hourText}:${minText}`;
